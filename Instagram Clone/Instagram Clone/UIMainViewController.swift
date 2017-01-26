b5b4117dd986388c71ee9b460cbdee73da6e6a4a
Edward Pie <edward@smsgh.com>
2017-01-26 10:34:37 +0000

    Change : Removes Main.storyboard and sets UIMainViewController instance and root view controller of application's window.

---
+++ Instagram Clone/Instagram Clone/UIMainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagram Clone
+//
+//  Created by Edward Pie on 26/01/2017.
+//  Copyright © 2017 Hubtel. All rights reserved.
+//
+
+import UIKit
+
+class UIMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
