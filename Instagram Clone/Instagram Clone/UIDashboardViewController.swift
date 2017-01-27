2040ba96a3d0dde92daa58a76154bbd7ce176b7f
Edward Pie <hackstockpie@gmail.com>
2017-01-27 08:01:16 +0000

    Change : Adds table view to dashboard to display users instagram feeds

--- Instagram Clone/Instagram Clone/UIDashboardViewController.swift
+++ Instagram Clone/Instagram Clone/UIDashboardViewController.swift
@@ -8,12 +8,55 @@
 
 import UIKit
 
-class UIDashboardViewController: UIViewController {
+class UIDashboardViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    let cellId = "FEED_CELL_ID"
+    
+    let feedsTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tableView
+    }()
+    
+    let activityIndicator: UIActivityIndicatorView = {
+        let activityIndicatorView = UIActivityIndicatorView()
+        activityIndicatorView.hidesWhenStopped = true
+        activityIndicatorView.activityIndicatorViewStyle = .gray
+        
+        return activityIndicatorView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.initializeViews()
+        self.applyLayoutConstraints()
+    
+    }
+    
+    func initializeViews(){
+        self.activityIndicator.center = self.view.center
+        self.feedsTableView.delegate = self
+        self.feedsTableView.dataSource = self
+        
+        self.feedsTableView.register(UITableViewCell.self, forCellReuseIdentifier: self.cellId)
+    }
+    
+    func applyLayoutConstraints(){
+        self.view.addSubview(self.feedsTableView)
+        self.feedsTableView.anchorToTop(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor)
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: self.cellId, for: indexPath) as UITableViewCell
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +64,5 @@ class UIDashboardViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
